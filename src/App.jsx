9bc64ec7eba085d068d2e2052486a40573475255
Tushar Amdoskar <tusharvamdoskar@gmail.com>
2026-02-14 07:19:12 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,9 @@ function App() {
     if (!name) {
       return (
       <div className='App' style={{display:'flex',    flexDirection:'column'}}>
+        <h1 className="text-3xl font-bold text-center text-pink-600 my-4">
+          Tell us your crush's name!
+        </h1>
         <input className="px-4 py-2 border border-yellow-100 rounded-lg outline-5 outline-red-300  focus:ring-5 focus:ring-pink-300 focus:border-pink-500 transition"/>
         <br/>
         <Button variant="primary" className='button'>Submit</Button>
