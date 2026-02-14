0365d7b1f4720a348d9eefd7b16bab7a4b7d3db5
Tushar Amdoskar <tusharvamdoskar@gmail.com>
2026-02-14 07:11:57 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Container, Row, Col, Card } from "react-bootstrap";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const personName = params.get("name");
+    console.log("Person Name from URL:", personName);
+    if (personName) setName(personName);
+  }, []);
+
+
+    if (!name) {
+      return (
+      <div className='App' style={{display:'flex',    flexDirection:'column'}}>
+        <input className="px-4 py-2 border border-yellow-100 rounded-lg outline-5 outline-red-300  focus:ring-5 focus:ring-pink-300 focus:border-pink-500 transition"/>
+        <br/>
+        <Button variant="primary" className='button'>Submit</Button>
+      </div>)
+    }
+
 }
 
 export default App
